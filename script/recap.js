2b038fcad14bb6a4f85f33b6bd2fcb610ab427c3
LuciaRomagnoli <lucia.romagnoli88@gmail.com>
2024-10-08 13:11:22 +0200

    modify

---
+++ script/recap.js
@@ -0,0 +1,15 @@
+console.log('Foglio java collegato correttamente')
+
+//variabili
+let thisIsAVariable = 'Stefano'
+//stringhe, numeri, booleani, undefined, null
+
+thisIsAVariable = 'Epicode'
+
+const smartphone = {
+  brand: 'Apple',
+  model: 'IPhone',
+  newModel: true,
+  capacity: 128,
+  color: 'yellow',
+}
